ba41c17f26fae887bf496c740e03798f19d42cbe
fahmyabdul <firstfahmyabdul@gmail.com>
2016-06-28 10:32:33 +0700

    Perubahan Template Main

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'personalweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'muhfahmy',
+        'PASSWORD': 'fahmyabdul21st',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
