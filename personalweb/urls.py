17ca2a4c92167a053147895ebb56cf03cfcdb5ac
fahmyabdul <firstfahmyabdul@gmail.com>
2016-07-01 11:06:46 +0700

    perubahan header icon dan main url

--- personalweb/urls.py
+++ personalweb/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
-    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'',include('blog.urls')),
-    url(r'^main/',include('mainweb.urls')),
+    url(r'^blog/accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^blog/accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/blog/'}),
+    url(r'',include('mainweb.urls')),
+    url(r'^blog/',include('blog.urls')),
 ]
