978c1b39813124391ce6e119bdc5379832d7843a
fahmyabdul <firstfahmyabdul@gmail.com>
2016-07-12 08:56:19 +0700

    Perubahan tampilan

--- mainweb/urls.py
+++ mainweb/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.main_view, name='main_view'),
-	url(r'^coba/$', views.main_coba, name='main_coba'),
+	url(r'^adm/$', views.main_adm, name='adm_main'),
+	url(r'^adm/profil/$', views.adm_profil, name='adm_profil'),
+	url(r'^adm/blog/$', views.adm_blog, name='adm_blog'),
 ]
--- mainweb/views.py
+++ mainweb/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
 from .models import Bagian
 
 def main_view(request):
 	bagians = Bagian.objects.all()
 	return render(request, 'mainweb/main_view.html', {'bagians':bagians})
 
-def main_coba(request):
-	return render(request, 'mainweb/main_coba.html')
+def main_adm(request):
+	return render(request, 'mainweb/main_adm.html')
+
+def adm_profil(request):
+	return render(request, 'mainweb/adm_profil.html')
+
+def adm_blog(request):
+	return render(request, 'mainweb/adm_blog.html')
