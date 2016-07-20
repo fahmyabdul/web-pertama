677f0917735d79d87fd4e55a2a6e18912d9526b2
fahmyabdul <firstfahmyabdul@gmail.com>
2016-07-20 16:49:39 +0700

    perubahan model login

--- mainweb/models.py
+++ mainweb/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.contrib.sessions.models import Session
 
 class Bagian(models.Model):
     bagian_id 		= models.AutoField(primary_key=True)
@@ -31,10 +32,27 @@ class Login_log(models.Model):
 	log_authid	= models.IntegerField()
 	log_date	= models.DateTimeField(default=timezone.now)
 	log_status	= models.IntegerField()
-	log_action	= models.TextField()
+	log_session	= models.TextField(blank=True, null=True)
 
 	def simpan(self):
 		self.save()
 
 	def __str__(self):
 		return self.log_authid
+
+def entrilog(sender, request, user, **kwargs):
+	userid = request.user.id
+	login_log = Login_log()
+	login_log.log_authid 	= userid
+	login_log.log_date 		= timezone.now()
+	login_log.log_status	= 1
+	login_log.log_session 	= request.session.session_key
+	login_log.save()
+
+def dellog(sender, request, user, **kwargs):
+	userid = request.user.id
+	login_log = Login_log.objects.filter(log_authid=userid)
+	login_log.delete()
+
+user_logged_in.connect(entrilog)
+user_logged_out.connect(dellog)
